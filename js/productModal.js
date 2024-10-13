a95182140fec9cf8f7dbcfc674a1c08a939e417e
Avigail Shklovsky <9013825@gmail.com>
2024-10-13 13:37:38 +0300

    add comments anf prettify

--- js/productModal.js
+++ js/productModal.js
@@ -1,35 +1,28 @@
 import data from './cards-collections.json' with { type: 'json' };
 
 export default function openModal(id,category){
-    
+    // find the current item
    let product=  data[category].find(item => item.catalogId === id);
    console.log(product);
-   
+   // make the modal visible
    let modal = document.getElementsByClassName('modal')[0];
    modal.style.display = "flex";
-
+  // create and append modal elements
    let modalProduct = document.getElementsByClassName('productModal')[0];
-   
    let text = document.createElement('div');
-   text.id='text';
-   console.log(text.id);
-   
+   text.id='text';   
 
    if(product.title){
-    console.log(product.title);
-    
    let title = document.createElement('p')
    title.innerHTML = product.title; 
    text.appendChild(title);
-    // modalProduct.appendChild(title)
-   }
+     }
    if(product.price){
     let price = document.createElement('span');
     price.innerHTML =  `מחיר: ${product.price}  ₪`;
     text.appendChild(price);
-    // modalProduct.appendChild(price);
    }
-
+   // 'text' contains the title and price
    modalProduct.appendChild(text);
 
    if(product.image){
@@ -37,30 +30,28 @@ export default function openModal(id,category){
     image.src = product.image;
     modalProduct.appendChild(image)
    }
-
-
-
 }
 
+// close the modal - called from button
 export function closeModal(){
+    // make the modal invisible
     let modal = document.getElementsByClassName('modal')[0];
     modal.style.display = "none";
-    
+    // delete the elements to prevent double content
     let parent = document.querySelector('.productModal'); 
-
     let image = parent.querySelector('img');
     let price = parent.querySelector('span');
     let title = parent.querySelector('p');
-
     if (image) image.remove();
     if (price) price.remove();
     if (title) title.remove();
     
 }
 
+// makes the closeModal function global
 window.closeModal = closeModal;
 
-
+// close the model when clicking out of the modal
 window.onclick = function(event) {
     let modal = document.getElementsByClassName('modal')[0];
     if (event.target == modal) {
