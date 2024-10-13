d4ff9b17bc5f4fe2ac4bc5f886eea88af4392a37
Shira Biton <shirabiton387@gmail.com>
2024-10-13 10:47:47 +0300

    feat(cart): add logic for adding items to cart (card.js)

--- js/cards.js
+++ js/cards.js
@@ -2,36 +2,50 @@ import data from './cards-collections.json' with { type: 'json' };
 
 const categories = [{ id: 0, name: "notebooks", collection: data.notebooks }, { id: 1, name: "craft", collection: data.craft }]
 
+
 function createCards(categoryId) {
     let cardList = document.querySelector("#card-list");
     const category = categories.find(category => category.id == categoryId);
     const collection = category ? category.collection : [];
 
-    const card = collection.map(card => {
-        return `<article>
+    collection.forEach(card => {
+        const cardElement = document.createElement('article');
+        cardElement.innerHTML = `
             <h2>${card.title}</h2>
             <img class="img-card" src="${card.image}" alt="${card.title}"></img>
             <span class="cart-price">            
-            <p id="price">₪${card.price}</p>
-            <span class="cart" title="הוספה לסל"><i class="iconify" data-icon="mynaui:cart-solid"></i></span>
-            </span>
-        </article > `
-    }).join('');
+                <p id="price">₪${card.price}</p>
+                <span class="cart-icon">
+                    <i class="iconify" data-icon="mynaui:cart-solid"></i>
+                </span>
+            </span>`;
 
-    cardList.innerHTML += card;
+        const addToCartBtn = cardElement.querySelector('.cart-icon');
+        addToCartBtn.addEventListener('click', () => addToCart(card));
 
-    let cartButtons = document.querySelectorAll(".cart");
-    cartButtons.forEach(btn => {
-        btn.addEventListener('click', addToCart);
-    });
+        cardList.appendChild(cardElement);
+    })
 }
 
-function addToCart() {
-    alert("addToCart");
+
+function addToCart(item) {
+    console.log("add to cart");
+
+    let itemsInCart = JSON.parse(localStorage.getItem('itemsInCart')) || [];
+    let itemIndex = itemsInCart.findIndex(element => element.item.catalogId === item.catalogId);
+
+    let itemAmount = 1;
+
+    if (itemIndex !== -1)
+        itemsInCart[itemIndex].amount += itemAmount;
+    else
+        itemsInCart.push({ item: item, amount: itemAmount });
+
+    localStorage.setItem('itemsInCart', JSON.stringify(itemsInCart));
+
+    const arr = JSON.parse(localStorage.getItem('itemsInCart'));
+    console.log(arr);
 }
 
-// function openModal(card) {
-//     // need to be filled
-// }
 
 window.onload = createCards(0);
